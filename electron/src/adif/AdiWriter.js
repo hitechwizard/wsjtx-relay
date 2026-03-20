bfe38a99140a60183e1ad0a2c611d214bfde4943
Jesse Jarrell <jjarrell@htsop.com>
2026-03-20 11:06:10 -0500

    Ignore certain fields stored with the qso

--- electron/src/adif/AdiWriter.js
+++ electron/src/adif/AdiWriter.js
@@ -42,6 +42,11 @@ class AdiWriter {
   }
 
   writeProperty(key, value) {
+    const ignoredFields = [ "logSubmissions" ];
+    if (key in ignoredFields) {
+      return;
+    }
+
     let outKey = key;
     if (key === '_id') outKey = 'app_cloudshack_id';
     else if (key === '_rev') outKey = 'app_cloudshack_rev';
